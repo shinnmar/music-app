9083b4d3fb7ca1108a65c596d1d6e4cd014d7414
Shirley R <srm.shirley@gmail.com>
2023-06-28 11:53:48 -0500

    Play and pause for playlist. Styles updated and added new playlist

--- src/js/main.js
+++ src/js/main.js
@@ -14,17 +14,27 @@ const likedIcons = document.querySelectorAll(".liked-icon");
 likedIcons.forEach((likedIcon) => {
   likedIcon.addEventListener("click", () => {
     likedIcon.classList.toggle("liked-song");
+    likedIcon.classList.add("pulse");
   });
 });
 
 let followBtn = document.getElementById("followBtn");
 followBtn.addEventListener("click", () => {
   followBtn.classList.toggle("btn-following");
-})
+});
 
 let followBtnMobile = document.getElementById("followBtnMobile");
 followBtnMobile.addEventListener("click", () => {
   followBtnMobile.classList.toggle("btn-following");
 });
 
+let playIcon = document.getElementById("playIconDesktop");
+playIcon.addEventListener("click", () => {
+  playIcon.classList.toggle("pause-icon");
+})
+
+let playIconMobile = document.getElementById("playIconMobile");
+playIconMobile.addEventListener("click", () => {
+  playIconMobile.classList.toggle("pause-icon");
+})
 
